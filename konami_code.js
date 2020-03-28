413d2e9ef7188ed3c2081b0b223be340cfd23ed9
funny-teacher-0837 <detinsley1s@gmail.com>
2020-03-28 02:21:21 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -15,6 +15,15 @@ function init() {
   // your code here
   let idx = 0;
   document.body.addEventListener("keydown", e => {
-    console.log(e)
+    if(e.key === codes[idx]) {
+      idx += 1;
+    }
+    else {
+      idx = 0;
+    }
+    if(idx === codes.length) {
+      alert('Great!');
+      idx = 0;
+    }
   })
 }
